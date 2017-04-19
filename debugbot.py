9196d4f0f6f3ab994bd78c2f148eef19fdeca435
niriikon <niriikon@gmail.com>
2017-04-19 06:01:42 +0300

    Fixed problems with writing and reading config file, fixed Wappu counter.

--- debugbot.py
+++ debugbot.py
@@ -7,53 +7,60 @@ import configparser
 import re
 from datetime import datetime, timedelta
 
-adminlist = ["mirv@otitsun.oulu.fi", "vinvin@otitsun.oulu.fi"]
-operatorlist = ["glukoosi@glukoosi.com", "piipari@otitsun.oulu.fi", "~kurre@kumikurre.com", "matti@otitsun.oulu.fi"]
-faggotlist = ["jsloth@otitsun.oulu.fi", "mirv@otitsun.oulu.fi"]
+adminlist = ["mirv@otitsun.oulu.fi", "sebu@sebbu.net"]
+operatorlist = ["vinvin@otitsun.oulu.fi"]
+faggotlist = ["jsloth@otitsun.oulu.fi", "sebu@sebbu.net"]
 default_url = ""
-time = 0
 delay = 24
-isFaggot = 0
-wappu_tulee = datetime(2017, 4, 20, 18, 0, 0)
+wappu_tulee = datetime(2017, 5, 1, 0, 0, 0)
 wapu_lopu = datetime(2017, 5, 2)
 
 def readConfig():
-    config = configparser.ConfigParser()
-    config.read("config.ini")
+    global adminlist
+    global operatorlist
+    global faggotlist
+    global default_url
+    global delay
+    global wappu_tulee
 
-    parse = config["USERS"]["Adminlist"]
+    config = configparser.ConfigParser()
+    config.read('botconfig.ini')
+    
+    parse = config.get('USERS', 'Adminlist')
     adminlist = parse.split(',')
-    parse = config["USERS"]["Operatorlist"]
+    parse = config.get('USERS', 'Operatorlist')
     operatorlist = parse.split(',')
-    parse = config["USERS"]["Faggotlist"]
+    parse = config.get('USERS', 'Faggotlist')
     faggotlist = parse.split(',')
 
-    default_url = config["SETTINGS"]["url"]
-    time = config["SETTINGS"]["time"]
-    delay = config["SETTINGS"]["delay"]
+    default_url = config.get('SETTINGS', 'Url')
+    delay = int(config.get('SETTINGS', 'Delay'))
+    wappulist = config.get('SETTINGS', 'Wappu').split(',')
+    wappu_tulee = datetime(*map(int, wappulist))
 
 def writeConfig():
     config = configparser.ConfigParser()
-    config["USERS"] = {
-        "Adminlist": parseList(adminlist),
-        "Operatorlist": parseList(operatorlist),
-        "Faggotlist": parseList(faggotlist)}
+    config.add_section('USERS')
+    config.set('USERS', 'Adminlist', parseList(adminlist))
+    config.set('USERS', 'Operatorlist', parseList(operatorlist))
+    config.set('USERS', 'Faggotlist', parseList(faggotlist))
 
-    config["SETTINGS"] = {
-        "url": default_url,
-        "time": time,
-        "delay": delay}
+    config.add_section('SETTINGS')
+    config.set('SETTINGS', 'Url', default_url)
+    config.set('SETTINGS', 'Delay', str(delay))
+    config.set('SETTINGS', 'Wappu',
+        "{0},{1},{2},{3},{4},{5}".format(wappu_tulee.year, wappu_tulee.month, wappu_tulee.day, wappu_tulee.hour, wappu_tulee.minute, wappu_tulee.second))
 
-    with open('config.ini', 'w') as configfile:
+    with open('botconfig.ini', 'w') as configfile:
         config.write(configfile)
     configfile.close()
 
-def parseList(list, i=0):
+def parseList(list_in, i=0):
     list_as_string = ""
-    while (i < len(list)):
-        list_as_string += "{:s},".format(list[i])
+    while (i < len(list_in)):
+        list_as_string += "{:s},".format(list_in[i])
         i += 1
-    return list_as_string
+    return list_as_string.rstrip(',')
 
 def joinChannel(socket, channels):
     for i in range(len(channels)):
@@ -82,7 +89,9 @@ def pasteLink(url="http://www.pornhub.com/video/random"):
     redirect_url = res.geturl()
     return redirect_url
 
-def getWappu(time_comp=datetime.now()):
+def getWappu(time_comp=""):
+    if (time_comp == ""):
+        time_comp = datetime.now()
     if (wappu_tulee > time_comp):
         #wappuun aikaa
         time_diff = wappu_tulee - time_comp
@@ -102,6 +111,13 @@ def getWappu(time_comp=datetime.now()):
             return "Wapu ei lopu"
 
 def runloop(socket):
+    global adminlist
+    global operatorlist
+    global faggotlist
+    global default_url
+    global delay
+    global wappu_tulee
+
     while True:
         try:
             response = str(socket.recv(4096),"UTF-8", "replace")
@@ -111,6 +127,7 @@ def runloop(socket):
             else:
                 message = ""
             print("{}\n".format(response))
+
 
             user = response[0].split('!')
             if (len(user) >= 2):
@@ -133,13 +150,14 @@ def runloop(socket):
                     while (i < len(response)):
                         operatorlist.append(response[i])
                         i += 1
-                #writeConfig()
+                    writeConfig()
 
                 elif (response[3] == ":!addadmin"):
                     i = 4
                     while (i < len(response)):
                         adminlist.append(response[i])
                         i += 1
+                    writeConfig()
 
                 elif (response[3] == ":!addfaggot"):
                     i = 4
@@ -147,6 +165,7 @@ def runloop(socket):
                         faggotlist.append(response[i])
                         print("Added {:s} to faggotlist".format(response[i]))
                         i += 1
+                    writeConfig()
 
                 elif (response[3] == ":!rmop"):
                     if (response[4].isdigit()):
@@ -156,6 +175,7 @@ def runloop(socket):
                         while (i < len(response)):
                             operatorlist.remove(response[i])
                             i += 1
+                    writeConfig()
 
                 elif (response[3] == ":!rmadmin"):
                     if (response[4].isdigit()):
@@ -165,6 +185,7 @@ def runloop(socket):
                         while (i < len(response)):
                             adminlist.remove(response[i])
                             i += 1
+                    writeConfig()
 
                 elif (response[3] == ":!rmfaggot"):
                     if (response[4].isdigit()):
@@ -176,6 +197,17 @@ def runloop(socket):
                             faggotlist.remove(response[i])
                             print("Removed {:s} from faggotlist".format(response[i]))
                             i += 1
+                    writeConfig()
+
+                elif (response[3] == ":!setlink"):
+                    default_url = response[4]
+                    writeConfig()
+                    print(default_url)
+
+                elif (response[3] == ":!setwappu"):
+                    wappulist = response[4].split(',') #2017,4,20,18,0,0
+                    wappu_tulee = datetime(*map(int, wappulist))
+                    writeConfig()
 
                 # TODO
                 #
@@ -189,22 +221,18 @@ def runloop(socket):
                 #set_frequency
                 #set_link
                 #lotd -CHECK
-                if (response[3] == ":!time"):
-                    setTime(response[4])
 
-                elif (response[3] == ":!freq"):
-                    setfreq(response[4])
-
-                elif (response[3] == ":!setlink"):
-                    setLink(response[4])
+                if (response[3] == ":!delay"):
+                    delay = response[4]
+                    writeConfig()
 
                 elif (response[3] == ":!linkplz"):
                     if (response[2] == username):
-                        url = "Requested by {:s}: {:s}".format(user[0].lstrip(':'), pasteLink())
+                        url = "Requested by {:s}: {:s}".format(user[0].lstrip(':'), pasteLink(default_url))
                     else:
-                        url = pasteLink()
-                    socket.send("PRIVMSG {:s} :{:s}\r\n".format("#pornonystavat", url).encode('utf-8'))
-                    #socket.send("PRIVMSG {:s} :{:s}\r\n".format("#otit.bottest", url).encode('utf-8'))
+                        url = pasteLink(default_url)
+                    #socket.send("PRIVMSG {:s} :{:s}\r\n".format("#pornonystavat", url).encode('utf-8'))
+                    socket.send("PRIVMSG {:s} :{:s}\r\n".format("#sebbutest", url).encode('utf-8'))
 
                 elif (response[3] == ":!wappu"):
                     socket.send("PRIVMSG {:s} :{:s}\r\n".format(response[2], getWappu()).encode('utf-8'))
@@ -235,17 +263,18 @@ def runloop(socket):
         except KeyboardInterrupt:
             print("\nGoodbye")
             return None
-
+        """
         except:
            pass
+        """
 
 
 if __name__ == "__main__":
     
-    IPaddress = "irc.oulu.fi"
+    IPaddress = "irc.nlnog.net"
     portNo = 6667
 
-    
+    """
     nick = "BOTSebbu"
     username = "BOTSebbu"
     realname = "Pornon ystaevae"
@@ -259,7 +288,7 @@ if __name__ == "__main__":
     realname = "Debuggaamisen ystaevae"
     hostname = "IRC"
     servername = "IRCnet"
-    """
+    
 
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.connect((IPaddress, portNo))
@@ -269,13 +298,17 @@ if __name__ == "__main__":
     clientSocket.send("USER {:s} {:s} {:s} :{:s}\r\n".format(username, hostname, servername, realname).encode('utf-8'))
     clientSocket.send("NICK {:s}\n".format(nick).encode('utf-8'))
 
-    clientSocket = joinChannel(clientSocket, ["#sebbutest", "#pornonystavat"])
-    #clientSocket = joinChannel(clientSocket, ["#sebbutest", "#otit.bottest"])
+    #clientSocket = joinChannel(clientSocket, ["#sebbutest", "#pornonystavat"])
+    clientSocket = joinChannel(clientSocket, ["#sebbutest"])
 
+    
+    readConfig()
+    """
     try:
         readConfig()
     except:
         pass
+    """
 
     runloop(clientSocket)
 
